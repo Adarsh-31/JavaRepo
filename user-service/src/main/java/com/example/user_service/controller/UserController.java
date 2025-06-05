1eefc608b112359cdd828ee3d9768004709dc081
Adarsh.Goswami <Adarsh.Goswami@TL456>
2025-06-06 01:00:25 +0530

    First Commit:

---
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.user_service.controller;
+
+import com.example.user_service.model.User;
+import com.example.user_service.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return service.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return service.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+    }
+}
